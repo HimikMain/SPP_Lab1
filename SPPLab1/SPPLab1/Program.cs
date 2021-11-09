8cbcd932186c3e6aa83bdbb2633efcfd640fc656
Anatol <64561080+HimikMain@users.noreply.github.com>
2021-11-10 00:30:04 +0300

    СПП Лаба №1

---
+++ SPPLab1/SPPLab1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace SPPLab1
+{
+    class Program
+    {
+		static int num = 1;
+		static object locker = new object();
+		static void count()
+		{
+			lock (locker)
+			{
+				int j = 1;
+				for (int i = 0; i < 3; i++)
+				{
+					Console.WriteLine("Поток №{0}: {1}", num, j);
+					j++;
+					Thread.Sleep(50);
+				}
+				num++;
+			}
+		}
+		static void Main(string[] args)
+        {
+            TaskQueue taskQueue = new TaskQueue(3);
+            for (int i = 0; i < 3; i++)
+            {
+                taskQueue.EnqueueTask(count);
+			}
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SPPLab1/SPPLab1/TaskQueue.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace SPPLab1
+{
+    public delegate void TaskDelegate();
+
+    class TaskQueue
+    {
+        static object locker = new object();
+        private BlockingCollection<TaskDelegate> Tasks = new BlockingCollection<TaskDelegate>(new ConcurrentQueue<TaskDelegate>());
+        private int queueCount;
+
+        public TaskQueue(int queueCount)
+        {
+            this.queueCount = queueCount;
+            for (int i = 0; i < queueCount; i++)
+            {
+                var thread = new Thread(threadWork) { IsBackground = true }; ;
+                thread.Start();
+            }
+        }
+
+        public void threadWork()
+        {
+            lock (locker)
+            {
+                while (true)
+                {
+                    var task = Tasks.Take();
+                    try
+                    {
+                        task();
+                    }
+                    catch (ThreadStateException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    catch (ThreadAbortException ex)
+                    {
+                        Thread.ResetAbort();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+
+        public void EnqueueTask(TaskDelegate task)
+        {
+            this.Tasks.Add(task);
+        }
+    }
+}
